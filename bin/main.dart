ec1cb532e76e85cf7c3b76762c76915aff584dc3
Aleksey Radionov <pretorean@gmail.com>
2019-09-27 17:40:28 +0300

    initial commit

---
+++ bin/main.dart
@@ -0,0 +1,13 @@
+import 'package:scrypted_social_api/scrypted_social_api.dart';
+
+Future main() async {
+  final app = Application<ScryptedSocialApiChannel>()
+      ..options.configurationFilePath = "config.yaml"
+      ..options.port = 8888;
+
+  final count = Platform.numberOfProcessors ~/ 2;
+  await app.start(numberOfInstances: count > 0 ? count : 1);
+
+  print("Application started on port: ${app.options.port}.");
+  print("Use Ctrl-C (SIGINT) to stop running the application.");
+}
---
+++ lib/scrypted_social_api.dart
@@ -0,0 +1,12 @@
+/// scrypted_social_api
+///
+/// A Aqueduct web server.
+library scrypted_social_api;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:aqueduct/aqueduct.dart';
+export 'package:aqueduct/managed_auth.dart';
+
+export 'channel.dart';
