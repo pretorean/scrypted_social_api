5d7bb2af317761c8c6a7390c2106377b04ddece0
Aleksey Radionov <pretorean@gmail.com>
2019-09-27 21:17:02 +0300

    users

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,11 +1,19 @@
 import '../scrypted_social_api.dart';
 
+// тип прав в системе
+enum UserType { admin, user }
+
+// состояние пользователя в системе
+enum UserState { active, banned, deleted }
+
+// пользователь системы
 class User extends ManagedObject<_User>
     implements _User, ManagedAuthResourceOwner<_User> {
   @Serialize(input: true, output: false)
   String password;
 }
 
+@Table(name: "users")
 class _User extends ResourceOwnerTableDefinition {
 /* This class inherits the following from ManagedAuthenticatable:
 
@@ -23,4 +31,28 @@ class _User extends ResourceOwnerTableDefinition {
 
   ManagedSet<ManagedAuthToken> tokens;
  */
+
+  @Column(indexed: true)
+  String firstName;
+
+  @Column(indexed: true)
+  String lastName;
+
+  @Column(indexed: true, nullable: true)
+  String midName;
+
+  @Column(indexed: true, nullable: true)
+  String phone;
+
+  @Column(indexed: true, nullable: true)
+  String email;
+
+  @Column(indexed: true, defaultValue: 'false')
+  bool subscribeNews;
+
+  @Column(omitByDefault: true, indexed: true, defaultValue: "'user'")
+  UserType systemRole;
+
+  @Column(omitByDefault: true, indexed: true, defaultValue: "'active'")
+  UserState state;
 }
